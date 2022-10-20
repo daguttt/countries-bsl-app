2d25d662d4c6791677c6dd8770ec189dea920a91
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-10-20 18:31:43 -0500

    fix: redirect to login when user is not logged in
    Changes made:
    - Redirect user to 'login' route when attempts to enter 'countries' route using browser search bar and is not logged in

--- src/app/guards/is-logged-in.guard.ts
+++ src/app/guards/is-logged-in.guard.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad } from '@angular/router';
+import { CanActivate, CanLoad, Router, UrlTree } from '@angular/router';
 import { isLoggedIn } from '../shared/utils/is-logged-in';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IsLoggedInGuard implements CanActivate, CanLoad {
-  canLoad(): boolean {
-    return isLoggedIn();
+  constructor(private router: Router) {}
+
+  canLoad(): boolean | UrlTree {
+    return this.isLoggedIn();
   }
 
-  canActivate(): boolean {
-    return isLoggedIn();
+  canActivate(): boolean | UrlTree {
+    return this.isLoggedIn();
+  }
+
+  private isLoggedIn(): boolean | UrlTree {
+    return isLoggedIn() || this.router.createUrlTree(['/login']);
   }
 }
