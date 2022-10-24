dcbbef613990e5ce19a38c9c8df3bd3800fc5133
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-10-24 17:19:21 -0500

    test: lazyload flag images with NgOptimizeImage

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 import { CountriesRoutingModule } from './countries-routing.module';
 import { PageCountriesComponent } from './pages/page-countries/page-countries.component';
 
 @NgModule({
   declarations: [PageCountriesComponent],
-  imports: [CommonModule, CountriesRoutingModule],
+  imports: [CommonModule, CountriesRoutingModule, NgOptimizedImage],
 })
 export class CountriesModule {}
